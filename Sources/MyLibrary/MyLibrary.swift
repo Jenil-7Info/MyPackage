7524604060d55917234ab694c69201a1f1c2176f
Jenil Rughani <jenil@seveninfosystem.com>
2023-11-29 17:15:05 +0530

    Added Print Statement in init().

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -2,6 +2,7 @@ public struct MyLibrary {
     public private(set) var text = "Hello, World!"
 
     public init() {
+        print(text)
     }
     
     static public func printStatement(_ statement: String) {
